28d44f44071d3b6c09039fcb1d84e0a4d0d5885f
AndresAlvarado9 <nalvaradoc1@est.ups.edu.ec>
2023-05-09 23:31:43 -0500

    cambios en el html

---
+++ src/app/pages/lista-contactos/lista-contactos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-contactos',
+  templateUrl: './lista-contactos.component.html',
+  styleUrls: ['./lista-contactos.component.scss']
+})
+export class ListaContactosComponent {
+
+}
