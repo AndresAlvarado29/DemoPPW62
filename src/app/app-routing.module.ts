c2a16e6e8dd906ec73af6cd0ef2b6c3369b0b0c8
AndresAlvarado9 <nalvaradoc1@est.ups.edu.ec>
2023-05-10 08:04:57 -0500

    Modificaciones del formulario y html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaContactosComponent } from './pages/lista-contactos/lista-contactos.component';
+import { HeaderComponent } from './template/header/header.component';
 
 const routes: Routes = [
-  {path:"paginas/ListaContactos", component: ListaContactosComponent}
+  {path:"paginas/ListaContactos", component: ListaContactosComponent},
+  {path:"template/header", component: HeaderComponent}
 ];
 
 @NgModule({
