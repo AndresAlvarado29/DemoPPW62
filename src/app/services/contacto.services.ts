f7c464d4dfdee0370a218d80fe70de0142f5b11d
AndresAlvarado9 <nalvaradoc1@est.ups.edu.ec>
2023-06-07 00:12:38 -0500

    intento de agregar firestore al proyecto

--- src/app/services/contacto.services.ts
+++ src/app/services/contacto.services.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Contacto } from '../domain/contacto';
+/*import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/compat/firestore';*/
 
 @Injectable({
   providedIn: 'root'
@@ -7,9 +8,20 @@ import { Contacto } from '../domain/contacto';
 export class ContactoService {
 
   contactos: Contacto[] = [];
- /*contactoRef: AngularFirestoreCollection<Contacto>;
-  constructor(private db: AngularFire) { }*/
+  /*contactoRef: AngularFirestoreCollection<Contacto>;
+  constructor(private db: AngularFirestore) {
+    this.contactoRef=this.db.collection('contactos');
+   }
 
+  saveFS(contacto: Contacto){
+    this.contactoRef.add(contacto)
+    .then(() => {
+      console.log('Datos subidos correctamente');
+    })
+    .catch((error) => {
+      console.error('Error al subir datos: ', error);
+    });
+  }*/
   save(contacto: Contacto){
   this.contactos.push(contacto) 
   console.log(this.contactos)   
